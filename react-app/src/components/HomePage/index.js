30b7078f241e8760daf30b235e7c64e7e31bd6c3
Marcus Kim <kim.marcus97@gmail.com>
2023-03-22 21:32:28 -0700

    started reducer for channels

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,7 +1,19 @@
-
+import './HomePage.css'
 function HomePage() {
   return (
-    <div>WELCOME TO THE HOME PAGE</div>
+    <div className="home-container">
+      <div className='home-sidebar-container'>
+        <div className='home-sidebar-heading-container'>
+          <div className='home-sidebar-heading-title'>App Academy</div>
+          <div className='home-sidebar-heading-button'></div>
+        </div>
+        <div className='home-sidebar-content'></div>
+      </div>
+      <div className='home-content-container'>
+
+      </div>
+    </div>
+
   )
 }
 
--- react-app/src/components/Navigation/UserNavigation.js
+++ react-app/src/components/Navigation/UserNavigation.js
@@ -1,21 +1,16 @@
 import ProfileButton from "./ProfileButton";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './UserNavigation.css'
 
 function UserNavigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user)
-  
+
   return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className="user-nav-container">
+			<div className="user-nav-logo">SlackLine</div>
+			<div className="user-nav-profile-button"><ProfileButton user={sessionUser} /></div>
+		</div>
 	);
 }
 
