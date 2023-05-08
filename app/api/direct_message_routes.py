17c717265aaf1e8719731de43d8a9027f20a561e
Marcus Kim <kim.marcus97@gmail.com>
2023-05-07 17:37:08 -0700

    create direct message backend done

--- app/api/direct_message_routes.py
+++ app/api/direct_message_routes.py
@@ -13,3 +13,30 @@ def get_direct_messages():
     data = [directMessage.to_dict() for directMessage in directMessages];
 
     return data;
+
+#TODO Delete a direct message
+@direct_message_routes.route('/<int:directMessageId>', methods=['DELETE'])
+@login_required
+def delete_directMessage(directMessageId):
+    directMessage = DirectMessage.query.get(directMessageId)
+
+    db.session.delete(directMessage)
+    db.session.commit()
+
+    return "Successfully Deleted"
+
+#TODO Create a direct message
+@direct_message_routes.route('/', methods=['POST'])
+@login_required
+def create_directMessage():
+    req_body = request.get_json()
+
+    dm = DirectMessage(
+        user1_id = req_body['user1_id'],
+        user2_id = req_body['user2_id']
+    )
+
+    db.session.add(dm)
+    db.session.commit()
+
+    return dm
--- react-app/src/store/directMessages.js
+++ react-app/src/store/directMessages.js
@@ -1,10 +1,22 @@
 // ACTIONS
-const GET_DMS = 'directMessages/dms/GET_ALL'
+const GET_DMS = 'directMessages/dms/GET_ALL';
+const DELETE_DM = 'directMessages/dms/DELETE';
+const CREATE_DM = 'directMessages/dms/CREATE';
 
 // ACTION CREATORS
 const actionGetAllDMS = (dms) => ({
   type: GET_DMS,
   payload: dms
+})
+
+const actionCreateDM = (dm) => ({
+  type: CREATE_DM,
+  payload: dm
+})
+
+const actionDeleteDM = (dm) => ({
+  type: DELETE_DM,
+  payload: dm
 })
 
 // THUNKS
@@ -14,6 +26,26 @@ export const thunkGetAllDMS = () => async (dispatch) => {
   if (response.ok) {
     const data = await response.json();
     dispatch(actionGetAllDMS(data));
+    return data;
+  }
+}
+
+export const thunkCreateDM = (dm) => async (dispatch) => {
+  const reponse = await fetch(`/api/direct_messages/`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: dm
+  })
+}
+
+export const thunkDeleteDM = (dm) => async (dispatch) => {
+  const response = await fetch(`/api/direct_messages/${dm.id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(actionDeleteDM(dm));
     return data;
   }
 }
@@ -32,6 +64,14 @@ export default function reducer(state = initialState, action) {
       action.payload.forEach(dm => {
         newState.dms[dm.id] = dm;
       })
+      return newState;
+    }
+    case DELETE_DM: {
+      const newState = {
+        dms: { ...state.dms },
+        gdms: { ...state.gdms }
+      };
+      delete newState.dms[action.payload.id];
       return newState;
     }
     default:
